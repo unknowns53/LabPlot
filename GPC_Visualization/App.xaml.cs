5faeebe1a32f46ad056a5f4f98c911f7180b57cc
unknowns53 <sodium.fog@gmail.com>
2026-04-24 13:10:37 +0900

    Initial commit

---
+++ GPC_Visualization/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GPC_Visualization
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
